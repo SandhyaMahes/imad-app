1d66d41053a52699a7b888151d585f1bf37b5aaf
SandhyaMahes <30985155+SandhyaMahes@users.noreply.github.com>
2017-09-03 20:02:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,24 +5,7 @@ button.onclick=function() {
     request.onreadystatechange=function()
       {if (request.readyState === XMLHttpRequest.DONE)
         {if (request.status === 200)
-          {var counter = request.responseText;
-           var span = document.getElementById("count");
-           span.innerHTML = counter.toString();
-           }
-    
-         }
-    
- 
-     };
-     
-request.open('GET', 'http://sandhyamaheswaran.imad.hasura-app.io/counter' ,true);
-request.send(null);
-};
-var nameInput=document.getElementById('name');
-     var name=nameInput.value;
-     var click=document.getElementById('submit');
-     click.onclick=function(){
-         var names=['name1','name2','name3','name4'];
+          {var names=['name1','name2','name3','name4'];
          var list='';
          for(var i=0 ; i<names.length ; i++)
         {
@@ -30,5 +13,20 @@ var nameInput=document.getElementById('name');
         }
          var ul=document.getElementById('ulist');
          ul.innerHTML = list;
+           }
+    
+         }
+    
+ 
+     };
+     
+request.open('GET', 'http://sandhyamaheswaran.imad.hasura-app.io/submit-name?name='+name ,true);
+request.send(null);
+};
+var nameInput=document.getElementById('name');
+     var name=nameInput.value;
+     var click=document.getElementById('submit');
+     click.onclick=function(){
+         
      };
     
