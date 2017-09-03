3e27a6af695fbeaf9896e3039a9a1f832d61718d
SandhyaMahes <30985155+SandhyaMahes@users.noreply.github.com>
2017-09-03 11:26:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,19 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function()
 {
- counter+=1;
- var span=document.getElementById("count");
- span.innerHTML=counter.toString();
+    var request=XMLHttpRequest();
+    request.onreadystatechange=function()
+    {if (request.readystate===XMLHttpRequest.DONE)
+    {if (request.status===200)
+    {var counter=request.responseText;
+    var span=document.getElementById("count");
+    span.innerHtml=counter.toString();
+    }
+    }
+    }
+    
+ 
 };
+request.open('GET','http://sandhyamaheswaran.imad.hasura-app.io',true);
+request.send(null);
